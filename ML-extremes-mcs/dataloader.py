5d4e2ddebe4f937713d7dae3a59c3abf421dd5a8
Maria Molina <hurricanemolina@gmail.com>
2020-10-06 01:26:32 -0600

    bug fixes -- esp masks

--- ML-extremes-mcs/dataloader.py
+++ ML-extremes-mcs/dataloader.py
@@ -1,9 +1,10 @@
 import numpy as np
-import keras
+from tensorflow.python.keras.utils.data_utils import Sequence
 import xarray as xr
+import dl_stats
 from config import main_path_003
 
-class DataGenerator(keras.utils.Sequence):
+class DataGenerator(Sequence):
     """
     Generates data for Keras training.
     
@@ -12,7 +13,7 @@ class DataGenerator(keras.utils.Sequence):
         path_dataID (str): Directory path to ID files.
         variable (str): Variable(s) string as a list.
         ens_num (str): CESM model run number (e.g., ``003``).
-        instant (boolean): Whether the variable is instantaneous or not. Defaults to ``False``.
+        h_num (str): Whether the variable is instantaneous or not. List of ``h3`` and/or ``h4``.
         height ()
         batch_size (int): Batch size for training. Defaults to ``32``.
         dim (tuple): Tuple of spatial dimensions of the variable patches. Defaults to ``(106,31)``.
@@ -20,8 +21,10 @@ class DataGenerator(keras.utils.Sequence):
         n_classes (int): Number of output features (or channels). Defaults to ``2``.
         shuffle (boolean): Whether to shuffle for training. Defaults to ``True``.
         
+    Based on tutorial/blog: https://stanford.edu/~shervine/blog/keras-how-to-generate-data-on-the-fly
+        
     """
-    def __init__(self, list_IDs, path_dataID, variable, ens_num, instant=False, height=None, 
+    def __init__(self, list_IDs, path_dataID, variable, ens_num, h_num, height=None, 
                  batch_size=32, dim=(106, 81), n_channels=1, n_classes=2, shuffle=True):
         """
         Initialization.
@@ -30,11 +33,7 @@ class DataGenerator(keras.utils.Sequence):
         self.path_dataID = path_dataID
         self.variable = variable
         self.ens_num = ens_num
-        self.instant = instant
-        if self.instant:
-            self.inst_str = 'h4'
-        if not self.instant:
-            self.inst_str = 'h3'    
+        self.h_num = h_num
         self.height = height
         self.batch_size = batch_size
         self.dim = dim
@@ -78,20 +77,28 @@ class DataGenerator(keras.utils.Sequence):
         y = np.empty((self.batch_size, *self.dim, 2), dtype=int)
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
-            for j, VAR in enumerate(self.variable):
+            for j, (VAR, HNUM) in enumerate(zip(self.variable, self.h_num)):
                 # Store sample(s)
                 if self.ens_num == '002':
-                    X[i,:,:,j] = xr.open_dataset(f"{self.path_dataID}/plev_{VAR}_hgt{self.height}_ID{ID}.nc")[VAR].values
+                    # X[i,:,:,j] = xr.open_dataset(f"{self.path_dataID}/plev_{VAR}_hgt{self.height}_ID{ID}.nc")[VAR].values
+                    raise Exception("Ensemble member 2 option not ready.")
                 if self.ens_num == '003':
-                    X[i,:,:,j] = xr.open_dataset(f"{self.path_dataID}/file003_{self.inst_str}_{VAR}_ID{ID}.nc")[VAR].values
+                    X[i,:,:,j] = xr.open_dataset(f"{self.path_dataID}/file003_{HNUM}_{VAR}_ID{ID}.nc")[VAR].values
                 # Store class
-            y = self.create_binary_mask(y, i)
+            y = self.create_binary_mask(y, i, ID)
         return X, y
     
-    def create_binary_mask(self, y, IDindx, mask_var='binary_tag'):
+    def create_binary_mask(self, y, indx, IDindx, mask_var='binary_tag'):
         """
         Create binary mask for cross entropy training.
+        
+        Args:
+            y (array): Labels to populate.
+            indx (int): Enumerated value representing location in label array.
+            IDindx (int): Mask file ID corresponding to training data.
+            mask_var (str): Mask variable name in presaved file.
+            
         """
-        y[IDindx,:,:,0] = xr.open_dataset(f"{self.path_dataID}/mask_ID{IDindx}.nc")[mask_var].values
-        y[IDindx,:,:,1] = np.ones(self.dim, dtype=int) - y[IDindx,:,:,0]
+        y[indx,:,:,0] = xr.open_dataset(f"{self.path_dataID}/mask_ID{IDindx}.nc")[mask_var].values
+        y[indx,:,:,1] = np.ones(self.dim, dtype=int) - y[indx,:,:,0]
         return y
