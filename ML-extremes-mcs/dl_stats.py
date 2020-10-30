c95b0c595a66f63d730917abf5544f2813ef7dc9
Maria Molina <hurricanemolina@gmail.com>
2020-10-30 16:37:17 -0600

    stats for dl data norm/standardizing

--- ML-extremes-mcs/dl_stats.py
+++ ML-extremes-mcs/dl_stats.py
@@ -2,20 +2,16 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import calendar
-from config import main_path_003
 
 def extract_train_stats(filepath, variable, htype, fileIDs):
-    
     """
     Extract the stats files for respective variable and train IDs.
-    
     Args:
         filepath (str): Path of stats and variable data.
         variable (str): Name of variable as a string.
         htype (str): String representing hourly treatment. 
                      E.g., ``h3`` for average or ``h4`` instantaneous.
         fileIDs (array): List of IDs being used for training.
-
     """
     ds = xr.open_dataset(f"{filepath}/stats_{htype}_{variable}.nc")
     avgs = ds['averages'][fileIDs]
@@ -23,3 +19,44 @@ def extract_train_stats(filepath, variable, htype, fileIDs):
     maxs = ds['maxs'][fileIDs]
     mins = ds['mins'][fileIDs]
     return avgs, sigs, maxs, mins
+
+def era5_train_stats(filepath, variable, fileIDs):
+    """
+    Extract the stats files for respective ERA5 variable and train IDs.
+    Args:
+        filepath (str): Path of stats and variable data.
+        variable (str): Name of ERA5 variable as a string.
+        fileIDs (array): List of IDs being used for training.
+    Returns:
+        Averages, standard deviations, minimums, and maximums.
+    """
+    ds = xr.open_dataset(f"{filepath}/stats_era5_{variable}.nc")
+    avgs = ds['averages'][fileIDs]
+    sigs = ds['sigma'][fileIDs]
+    maxs = ds['maxs'][fileIDs]
+    mins = ds['mins'][fileIDs]
+    return avgs, sigs, mins, maxs
+
+def z_score(da, avgs, stds):
+    """
+    Compute z-score for training data.
+    Normalizing data for ML model training.
+    Args:
+        da (array): Array of training variable.
+        avgs (array): Array of averages for training variable.
+        stds (array): Array of standard deviations for training variable.
+    """
+    return (da - np.nanmean(avgs))/np.nanstd(stds)
+
+def min_max_scale(da, mins, maxs):
+    """
+    Scale training data by minimum and maximum values.
+    Args:
+        da (array): Array of training variable.
+        mins (array): Array of minimum values for training variable.
+        maxs (array): Array of maximum values for training variable.
+    """
+    if np.nanmax(maxs) == np.nanmin(mins):
+        raise Exception("Max and min are equal to each other!")
+    if np.nanmax(maxs) != np.nanmin(mins):
+        return (da - np.nanmin(mins))/(np.nanmax(maxs) - np.nanmin(mins))
