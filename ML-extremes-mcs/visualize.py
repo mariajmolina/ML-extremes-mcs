0b58be9ffb514884c9715ac92219e458a21367dc
Maria Molina <hurricanemolina@gmail.com>
2021-03-31 18:21:30 -0600

    continued removal of nonera5 or 1H code

--- ML-extremes-mcs/visualize.py
+++ ML-extremes-mcs/visualize.py
@@ -19,6 +19,7 @@ def create_STATES(us_states_location):
     
     Args:
         us_states_location (str): Directory location of states shapefiles.
+    
     Returns:
         States data as cartopy feature for plotting.
     """
@@ -47,6 +48,7 @@ def print_mask_info(data):
     
     Args:
         data (Xarray dataset): File containing masks.
+    
     Returns:
         Printed statements of total number of times (3-hourly for CESM CAM files)
         containing mcss.
@@ -56,45 +58,9 @@ def print_mask_info(data):
     print(np.argwhere(data.binary_tag.sum(axis=1).sum(axis=1).values!=0).reshape(-1))
     return
 
-def create_003onelev_plot(data, data_mask, time_indx, variable, STATES, cmap='viridis', 
-                          vmin=None, vmax=None):
-    """
-    Function to plot the variable data (CESM).
-    These are the 003 member plots that are on one level (e.g., QBOT, VBOT).
-    Dimensions of data variables must be named "time, lat, lon".
-    
-    Args:
-        data (Xarray dataset): The file contianing CESM variable.
-        data_mask (Xarray dataset): The file containing masks.
-        time_indx (int): Integer of time index.
-        variable (str): The name of the variable in the file.
-        STATES (cartopy feature): US STATES file.
-        cmap (str): Colormap name option from matplotlib. Defaults to ``viridis``.
-        vmin (float): Set minimum for plot. Defaults to ``None``.
-        vmax (float): Set maximum for plot. Defaults to ``None``.
-    Returns:
-        Plot of the respective variable.
-    """
-    fig = plt.figure(figsize=(6.,4.))
-    ax = plt.axes([0.,0.,1.,1.], projection=ccrs.PlateCarree())
-    data_isel = data.isel(
-               time=time_indx,
-               lat=slice(np.where(data.lat.values==data_mask.lat[0].values)[0][0],
-                         np.where(data.lat.values==data_mask.lat[-1].values)[0][0]+1),
-               lon=slice(np.where(data.lon.values==data_mask.lon[0].values)[0][0],
-                         np.where(data.lon.values==data_mask.lon[-1].values)[0][0]+1))
-    data_isel[variable].plot.pcolormesh(ax=ax, transform=ccrs.PlateCarree(), 
-                                        cmap=cmap, vmin=vmin, vmax=vmax)
-    ax.add_feature(STATES, facecolor='none', edgecolor='k', zorder=30)
-    ax.add_feature(cf.BORDERS)
-    ax.margins(x=0,y=0)
-    ax.coastlines()
-    return plt.show()
-
 def create_mask_plot(data, time_indx, STATES, cmap="Reds", lat='lat', lon='lon'):
     """
     Function to plot MCS masks.
-    
     ** Works with original tempestextremes files.
     
     Args:
@@ -102,6 +68,7 @@ def create_mask_plot(data, time_indx, STATES, cmap="Reds", lat='lat', lon='lon')
         time_indx (int): Integer index of mask time.
         STATES (cartopy feature): US STATES file.
         cmap (str): Colormap name option from matplotlib. Defaults to ``Reds``.
+    
     Returns:
         Plot of mask with state borders drawn for a respective time frame.
     """
@@ -116,56 +83,12 @@ def create_mask_plot(data, time_indx, STATES, cmap="Reds", lat='lat', lon='lon')
     ax.margins(x=0,y=0)
     ax.coastlines()
     return plt.show()
-
-def create_002multilev_plot(data, data_mask, time_indx, hght_indx, variable, STATES,
-                             cmap='viridis', vmin=None, vmax=None, dpi=200, savedir=None):
-    """
-    Function to plot the variable data.
-    These are the 002 member plots that are on several levels (e.g., V, U).
-    Dimensions of data variables must be named "ncl4, ncl5, ncl6, ncl7" for time, height, lat, lon respectively.
-
-    Args:
-        data (Xarray dataset): The file contianing CESM variable.
-        data_mask (Xarray dataset): The file containing masks.
-        time_indx (int): Integer of time index.
-        hght_indx (int): Integer of height index.
-        variable (str): The name of the variable in the file.
-        STATES (cartopy feature): US STATES file.
-        savedir (str): Directory to save the figure in.
-        cmap (str): Colormap name option from matplotlib. Defaults to ``viridis``.
-        vmin (float): Set minimum for plot. Defaults to ``None``.
-        vmax (float): Set maximum for plot. Defaults to ``None``.
-    Returns:
-        Plot of the respective variable.
-    """
-    data = data.assign_coords(ncl6=("ncl6",data.lat.values),
-                              ncl7=("ncl7",data.lon.values))
-    fig = plt.figure(figsize=(6.,4.))
-    ax = plt.axes([0.,0.,1.,1.], projection=ccrs.PlateCarree())
-    data.isel( ncl4=time_indx,
-               ncl5=hght_indx,
-               ncl6=slice(np.where(data.lat.values==data_mask.lat[0].values)[0][0],
-                          np.where(data.lat.values==data_mask.lat[-1].values)[0][0]+1),
-               ncl7=slice(np.where(data.lon.values==data_mask.lon[0].values)[0][0],
-                          np.where(data.lon.values==data_mask.lon[-1].values)[0][0]+1))[variable].plot.pcolormesh(
-        ax=ax, transform=ccrs.PlateCarree(), 
-        cmap=cmap, vmin=vmin, vmax=vmax)
-    ax.add_feature(STATES, facecolor='none', edgecolor='k', zorder=30)
-    ax.add_feature(cf.BORDERS)
-    ax.margins(x=0,y=0)
-    ax.coastlines()
-    if not savedir:
-        return plt.show()
-    if savedir:
-        plt.savefig(f"{savedir}/mcsquad_{time_indx}.png", bbox_inches='tight', dpi=dpi)
-        plt.close()
         
 def create_training_plot(data, variable, STATES, lat='lat', lon='lon',
                          cmap='viridis', vmin=None, vmax=None, 
                          dpi=200, savedir=None, indx=0):
     """
     Function to plot the variable data already processed for training.
-    
     ** Works with processed tempestextremes MASKs (with ID nums) and 
     ** flextrkr files (if .isel(time=0) and variable=cloudtracknumber).
     
@@ -176,6 +99,7 @@ def create_training_plot(data, variable, STATES, lat='lat', lon='lon',
         cmap (str): Colormap name option from matplotlib. Defaults to ``viridis``.
         vmin (float): Set minimum for plot. Defaults to ``None``.
         vmax (float): Set maximum for plot. Defaults to ``None``.
+    
     Returns:
         Plot of the respective variable.
     """
@@ -193,51 +117,11 @@ def create_training_plot(data, variable, STATES, lat='lat', lon='lon',
         plt.savefig(f"{savedir}/trainplot_{variable}_{indx}.png", bbox_inches='tight', dpi=dpi)
         plt.close()
 
-def create_002onelev_plot(data, data_mask, time_indx, variable, STATES, cmap='viridis', 
-                          vmin=None, vmax=None, dpi=200, savedir=None):
-    """
-    Function to plot the variable data.
-    These are the 002 member plots that are on one level (e.g., CAPE).
-    Dimensions of data variables must be named "time, lat, lon".
-
-    Args:
-        data (Xarray dataset): The file contianing CESM variable.
-        data_mask (Xarray dataset): The file containing masks.
-        time_indx (int): Integer of time index.
-        variable (str): The name of the variable in the file.
-        STATES (cartopy feature): US STATES file.
-        savedir (str): Directory to save the figure in.
-        cmap (str): Colormap name option from matplotlib. Defaults to ``viridis``.
-        vmin (float): Set minimum for plot. Defaults to ``None``.
-        vmax (float): Set maximum for plot. Defaults to ``None``.
-    Returns:
-        Plot of the respective variable.
-    """
-    data = data.assign_coords(ncl6=("ncl6", data.lat.values),
-                              ncl7=("ncl7", data.lon.values))
-    fig = plt.figure(figsize=(6.,4.))
-    ax = plt.axes([0.,0.,1.,1.], projection=ccrs.PlateCarree())
-    data.isel( ax=ax, transform=ccrs.PlateCarree(), 
-               time=time_indx,
-               lat=slice(np.where(data.lat.values==data_mask.lat[0].values)[0][0],
-                         np.where(data.lat.values==data_mask.lat[-1].values)[0][0]+1),
-               lon=slice(np.where(data.lon.values==data_mask.lon[0].values)[0][0],
-                         np.where(data.lon.values==data_mask.lon[-1].values)[0][0]+1))[variable].plot.pcolormesh(
-        cmap=cmap, vmin=vmin, vmax=vmax)
-    ax.add_feature(STATES, facecolor='none', edgecolor='k', zorder=30)
-    ax.add_feature(cf.BORDERS)
-    ax.margins(x=0,y=0)
-    ax.coastlines()
-    if not savedir:
-        return plt.show()
-    if savedir:
-        plt.savefig(f"{savedir}/mcs_002onelev_{time_indx}.png", bbox_inches='tight', dpi=dpi)
-        plt.close()
-
 def create_mcs_stat_plot(data, STATES, lat, lon, vmin=0, vmax=10, cmap='BuPu', title=None):
     """
     Function to plot MCS stats on maps.
     ** Works with nontracked_total_grid and tracked_total_grid.
+    
     Args:
         data (2d numpy array): The file containing stats.
         STATES (cartopy feature): US STATES file.
@@ -264,6 +148,7 @@ def create_mcs_stat_figure(data, STATES, lat, lon, vmin=0, vmax=10, cmap='BuPu',
     """
     Function to plot MCS stats on maps.
     ** Works with nontracked_total_grid and tracked_total_grid.
+    
     Args:
         data (list of 2d numpy array): The files containing stats.
         STATES (cartopy feature): US STATES file.
