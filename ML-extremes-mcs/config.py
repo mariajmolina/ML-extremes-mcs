9f54ab7b1720cdef6ec6516233df56aca68ee085
Maria Molina <hurricanemolina@gmail.com>
2021-07-07 13:06:12 -0600

    update config

---
+++ ML-extremes-mcs/config.py
@@ -0,0 +1,18 @@
+import os
+
+start_year = '2004'
+end_year = '2019'
+
+main_path_era = '/glade/scratch/molina/cesm_mcs/cesm_era5/'
+mcsera_obs = '/glade/scratch/molina/cesm_mcs/mcs_flextrkr_era5/'
+
+era_obs = '/glade/collections/rda/data/ds633.0/'
+
+us_states_location = '/glade/work/molina/DATA/cartopy_usstates/'
+savefig_path = '/glade/work/molina/DATA/temp_figs/'
+
+# previous paths
+# main_path_002 = '/glade/scratch/molina/cesm_mcs/cesm_002/'
+# main_path_003 = '/glade/scratch/molina/cesm_mcs/cesm_003/'
+# main_path_radar = '/glade/scratch/molina/cesm_mcs/cesm_radar/'
+# radar_obs = '/glade/scratch/molina/cesm_mcs/mcs_flextrkr/'
